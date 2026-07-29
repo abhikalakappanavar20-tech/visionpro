6979be49a1747141ad37d73f6faeda290e748483
abhi <abhikalakappanavar20@gmail.com.com>
2026-07-29 19:04:04 +0530

    Switch to Vercel Django auto-detection with pyproject.toml

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VeriVision.settings')
-
-from VeriVision.wsgi import application
-
-app = application
