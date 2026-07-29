252eb0c3afa5cf75ae5ca1b850d76f876ee7e9b6
abhi <abhikalakappanavar20@gmail.com.com>
2026-07-29 19:13:34 +0530

    Run migrations at startup for in-memory SQLite

--- api/index.py
+++ api/index.py
@@ -4,6 +4,11 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VeriVision.settings')
 
+from django.core.management import call_command
+from django.conf import settings
+
+call_command('migrate', '--noinput', verbosity=0)
+
 from VeriVision.wsgi import application
 
 app = application
